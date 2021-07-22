e886d20f2442df18a9ee053479df732b1dcb9bba
Axel Junior <alexandre_junior@id.uff.br>
2021-07-22 01:10:34 -0300

    v0.6
    Algumas funções refatoradas e terminei de colocar a parte de calculo da posições para onde as box devem ir

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ function buildGameObjects(){
         else if (i==3 && j===2)
             test=4
         return {
-        box: boxes[cont],
-        innerValue: test,
-        coordX: i,
-        coordY: j
+            box: boxes[cont],
+            innerValue: test,
+            coordX: i,
+            coordY: j
         }
     }
 
@@ -40,11 +40,140 @@ function buildGameObjects(){
 function catchEvent(event){
     const keyPressed = event.keyCode
     if(keyPressed === 87 )// W
-        updateGameTable("ToUp")
-    if(keyPressed === 83) //S
-        updateGameTable("ToDown")
+        calculateGameTable("ToUp")
+    else if(keyPressed === 65) //A
+            calculateGameTable("ToLeft")
+    else if(keyPressed === 83) //S
+        calculateGameTable("ToDown")
+    else if(keyPressed === 68) //D
+        calculateGameTable("ToRight")
 }
 // 87 W 83 S 65 A 68 D
+function calculateGameTable(orientation){
+    const boxColumnList = []
+    const staticBordVal = (orientation == "ToUp" || orientation == "ToLeft" ? 0 : 3)
+    if(orientation === "ToUp" || orientation === "ToDown"){
+        //bordVal = (orientation == "ToUp" ? 0 : 3)
+        for (const column of Array(4).keys()) {
+            boxColumnList[column] = gameObjectsArray.filter(item => (item.coordX == column && item.coordY != staticBordVal))
+        }
+    }else if(orientation === "ToLeft" || orientation === "ToRight"){
+        //bordVal = (orientation == "ToLeft" ? 0 : 3)
+        for (const row of Array(4).keys()) {
+            boxColumnList[row] = gameObjectsArray.filter(item => (item.coordX != staticBordVal && item.coordY == row ))
+        }
+    }
+    moveTable()
+    function moveTable(){
+        //let boxDirectionRef = 0
+        for (const variableBordVal of Array(4).keys()) {
+            if (orientation === "ToDown" || orientation === "ToRight"){
+                const reverseBoxColumnList = boxColumnList[variableBordVal].slice().reverse()
+                //console.log(reverseBoxColumnList) // ====================
+                reverseBoxColumnList.forEach(box => {
+                    const boxDirectionRef = (orientation === "ToDown"  ? box.coordY+1 : box.coordX+1)
+                    if (box.innerValue){
+                        // console.log(box.coordX,box.coordY, "A borda esta em",variableBordVal,staticBordVal) // ====================
+                        for (let i=boxDirectionRef; i<=3; i++){
+                            //console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",box.coordX,i) // ====================
+                            // orientation === "ToDown"  ? console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",box.coordX,i) : console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",i,box.coordY)  // ====================
+                            orientation === "ToDown"  ? 
+                            updateBoxStatus(box,box.coordX,i,variableBordVal,staticBordVal) : 
+                            updateBoxStatus(box,i,box.coordY,staticBordVal,variableBordVal)
+                            //updateBoxStatus(box,box.coordX,i,xBord,yBord)
+                        }
+                    }   
+                })
+            }else if (orientation === "ToUp" || orientation === "ToLeft"){
+                //console.log(reverseBoxColumnList) // ====================
+                boxColumnList[variableBordVal].forEach(box => {
+                    const boxDirectionRef = (orientation === "ToUp" ? box.coordY-1 : box.coordX-1)
+                    if (box.innerValue){
+                        // console.log(box.coordX,box.coordY, "A borda esta em",variableBordVal,staticBordVal) // ====================
+                        for (let i=boxDirectionRef; i>=0; i--){
+                            //console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",box.coordX,i) // ====================
+                            // orientation == "ToUp"  ? console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",box.coordX,i) : console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",i,box.coordY)  // ====================
+                            orientation == "ToUp"   ? 
+                            updateBoxStatus(box,box.coordX,i,variableBordVal,staticBordVal) : 
+                            updateBoxStatus(box,i,box.coordY,staticBordVal,variableBordVal)
+                            //updateBoxStatus(box,box.coordX,i,xBord,yBord)
+                        }
+                    }   
+                })
+            } 
+        }
+    }
+    function updateBoxStatus(box,xTarget,yTarget,xBord,yBord){
+        gameObjectsArray.forEach((cell,index) => {
+            if(cell.coordX == xTarget && cell.coordY == yTarget)
+                if (box.innerValue === cell.innerValue)
+                    console.log("Merge as duas box")
+                    // Limpa os dados de Box e soma com os de cell
+                else if(cell.innerValue)
+                    console.log("Move para a posição anterior a",cell.coordX,cell.coordY)
+                else if(cell.coordX == xBord && cell.coordY == yBord){
+                    console.log(box.coordX,box.coordY,"Foi para", xBord,yBord)
+                    // Copia os dados de Box para Cell e limpa Box
+            }
+        })
+    }
+}
+
+start()
+
+for(let i=0;i<16;i++){
+    console.log(gameObjectsArray[i].innerValue,gameObjectsArray[i].box,gameObjectsArray[i].coordX,gameObjectsArray[i].coordY)
+}
+
+
+/*
+// ===================== Segunda ideia funcional ======================= //
+function calculateGameTable(orientation){
+    const boxColumnList = []
+    let bordVal = 0
+    if(orientation === "ToUp" || orientation === "ToDown"){
+        bordVal = (orientation == "ToUp" ? 0 : 3)
+        for (const column of Array(4).keys()) {
+            boxColumnList[column] = gameObjectsArray.filter(item => (item.coordX == column && item.coordY != bordVal))
+        }
+    }else if(orientation === "ToLeft" || orientation === "ToRight"){
+        bordVal = (orientation == "ToLeft" ? 0 : 3)
+        for (const row of Array(4).keys()) {
+            boxColumnList[row] = gameObjectsArray.filter(item => (item.coordX != bordVal && item.coordY == row ))
+        }
+    }
+    moveInTable(bordVal)
+    function moveInTable(yBord){
+        for (const xBord of Array(4).keys()) {
+            const reverseBoxColumnList = boxColumnList[xBord].slice().reverse()
+            if (orientation === "ToDown" || orientation === "ToRight"){
+                console.log(reverseBoxColumnList)
+                reverseBoxColumnList.forEach(box => {
+                    if (box.innerValue){
+                        console.log(box.coordX,box.coordY, "A borda esta em",xBord,yBord)
+                        for (let i=box.coordY+1; i<=3; i++){
+                            console.log("Minha posição",box.coordX,box.coordY,"Posições obeservada",box.coordX,i)
+                            updateBoxPosition(box,box.coordX,i,xBord,yBord)
+                        }
+                    }   
+                })
+            }        
+        }
+    }
+    function updateBoxPosition(box,xTarget,yTarget,xBord,yBord){
+        gameObjectsArray.forEach(cell => {
+            if(cell.coordX == xTarget && cell.coordY == yTarget)
+                if (box.innerValue === cell.innerValue){
+                    console.log("Merge as box")
+                }else if(cell.innerValue){
+                    console.log("Move para a posição anterior a esta")
+                }else if(cell.coordX == xBord && cell.coordY == yBord){
+                    console.log(box.coordX,box.coordY,"Foi para", xBord,yBord)
+                }
+        })
+    }
+}
+// ================= Primeira ideia funcional ==================== //
 function updateGameTable(orientation){
     if(orientation === "ToUp"){
         for (const n of Array(4).keys()) {
@@ -61,16 +190,21 @@ function updateGameTable(orientation){
         for (const n of Array(4).keys()) {
             gameObjectsArray.slice().reverse().forEach(box => {
                 if((box.coordX == n && box.coordY != 3) && box.innerValue){
-                    console.log(box.box,box.coordX,box.coordY)
-                    console.log("Minha posição", box.coordX,box.coordY,"Preciso percorrer: ")
+                    //console.log(box.box,box.coordX,box.coordY)
+                    //console.log("Minha posição", box.coordX,box.coordY,"Preciso percorrer: ")
                     for (let column=box.coordY+1; column<=3; column++){
-                        console.log(box.coordX,column)
+                        //console.log(box.coordX,column)
                         updateBoxStatus(box,column,n,3)
                     }
                 }
             })
         }
+    }else if(orientation === "ToRight"){
+        const columnsBoxList = gameObjectsArray.filter(item => ((item.coordX != 3 && item.coordY == 3)))
+        console.log(columnsBoxList)
+
     }
+
     function updateBoxStatus(box,positionInColumn,xBord,yBord){
         gameObjectsArray.forEach(element => {
             if(element.coordX == box.coordX && element.coordY == positionInColumn){
@@ -85,36 +219,10 @@ function updateGameTable(orientation){
         })
     }
 }
-
-start()
-
-for(let i=0;i<16;i++){
-    console.log(gameObjectsArray[i].innerValue,gameObjectsArray[i].box,gameObjectsArray[i].coordX,gameObjectsArray[i].coordY)
-}
-
-
-
-/*
-let cont=0
-for(let i=0;i<4;i++){
-    for(let j=0;j<4;j++){
-        gameObjectArray.push(gameObjectFactory(cont,i,j))
-        cont++;
-    }
-}
-
-const boxes = document.getElementsByClassName("box")
-for(let i=0;i<16;i++){
-    console.log(boxes[i])
-}
-const elemento = getRandomInt(0,16)
-boxes[elemento].classList.add("filled") M-- THIS HERE
+// ============================================================== //
 */
 
-
 /*
-if (index === 4)
-(filho as HTMLElement).classList.add("selected")
-else
-(filho as HTMLElement).classList.remove("selected")
+const elemento = getRandomInt(0,16)
+boxes[elemento].classList.add("filled") M-- THIS HERE
 */
